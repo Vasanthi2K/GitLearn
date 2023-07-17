0cfa63e70188e9cf7e902a1553e1acb31013dec8
Vasanthi2K <70060435+Vasanthi2K@users.noreply.github.com>
2023-07-17 22:16:15 +0530

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -39,7 +39,58 @@ for b in range(b,0,-1):
     result *= a
 print(result)
 '''
+'''
 a=int(input())
 b=int(input())
 result = pow(a,b)
 print(result)
+'''
+'''
+Printing a num in reverse order
+num = int(input())
+reversed_num = 0
+
+while num != 0:
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10
+
+print("Reversed Number: " + str(reversed_num))
+'''
+
+'''
+num=int(input())
+print(str(num)[::-1])
+'''
+
+'''
+Fibonacci Series
+num= int(input())
+a=0
+b=1
+if num == 1:
+    print(a)
+else :
+    print(a)
+    print(b)
+    for i in range(2, num):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+'''
+'''
+Program to check whether a num is prime or not
+
+num=int(input())
+if num>1:
+    for i in range(2,num):
+        if (num%i)==0:
+            print(num, "is not a prime num")
+            break
+    else:
+        print(num, "is a prime")
+else:
+    print(num, "is not a prime")
+
+'''
